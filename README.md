f88926bbb1c770440089fae4fe649b45d8048ad1
atiquzzaman_soikat <atik.aiub@gmail.com>
2017-12-17 17:15:07 +0600

    added  webpack config for static page development

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
+
+# start serving static pages, for builing static html pages with hot reload
+npm run static
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ build/webpack.static.conf.js
@@ -0,0 +1,50 @@
+const config = require('../config')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "style.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+context: path.resolve(__dirname, '../'),  
+  entry: {
+      sass: './src/assets/sass/style.scss'
+  },
+  output: {
+    filename: 'dummy.js',
+    path: config.build.assetsRoot,
+  },
+  watch: true,
+  module: {
+    rules: [
+        {
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new BrowserSyncPlugin({
+        // browse to http://localhost:3000/ during development, 
+        // ./ directory is being served 
+        host: 'localhost',
+        port: 3000,
+        files: ['./static/*.html'],
+        server: { baseDir: ['./static','./'] }
+      })
+  ]
+};
